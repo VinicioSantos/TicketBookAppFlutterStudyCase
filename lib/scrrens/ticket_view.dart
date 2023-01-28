6485e88be774e28e5a8ddcd260a7e8bae3b3f114
Vinicio  Costa Santos <vinicio.csantos@outlook.com>
2023-01-28 18:39:58 -0300

    top side of the ticker widget

--- lib/scrrens/ticket_view.dart
+++ lib/scrrens/ticket_view.dart
@@ -4,6 +4,9 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_application_1/utils/app_layout.dart';
 import 'package:flutter_application_1/utils/app_styles.dart';
+import 'package:gap/gap.dart';
+
+import '../widgets/thick_container.dart';
 
 class TicketView extends StatelessWidget {
   const TicketView({Key? key}) : super(key: key);
@@ -26,11 +29,71 @@ class TicketView extends StatelessWidget {
           child: Column(children: [
             Row(
               children: [
-                Text("Nyc",
+                Text("NYC",
                     style: Styles.headLineStyle3.copyWith(color: Colors.white)),
+                Expanded(child: Container()),
+                ThickContainer(),
+                Expanded(
+                    child: Stack(
+                  children: [
+                    SizedBox(
+                      height: 24,
+                      child: LayoutBuilder(
+                        builder:
+                            (BuildContext builder, BoxConstraints constraints) {
+                          return Flex(
+                            direction: Axis.horizontal,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            mainAxisSize: MainAxisSize.max,
+                            children: List.generate(
+                                (constraints.constrainWidth() / 6).floor(),
+                                (index) => SizedBox(
+                                    width: 3,
+                                    height: 1,
+                                    child: DecoratedBox(
+                                      decoration: BoxDecoration(
+                                          color: Colors.white.withOpacity(0.5)),
+                                    ))),
+                          );
+                        },
+                      ),
+                    ),
+                    Center(
+                      child: Transform.rotate(
+                        angle: 1.5,
+                        child: Icon(Icons.local_airport_rounded,
+                            color: Colors.white),
+                      ),
+                    ),
+                  ],
+                )),
+                ThickContainer(),
                 const Spacer(),
-                Text("London",
+                Text("LDN",
                     style: Styles.headLineStyle3.copyWith(color: Colors.white)),
+              ],
+            ),
+            const Gap(3),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                    width: 100,
+                    child: Text(
+                      'New-York',
+                      style:
+                          Styles.headLineStyle4.copyWith(color: Colors.white),
+                    )),
+                Text("8H 30M",
+                    style: Styles.headLineStyle4.copyWith(color: Colors.white)),
+                SizedBox(
+                    width: 100,
+                    child: Text(
+                      'London',
+                      textAlign: TextAlign.end,
+                      style:
+                          Styles.headLineStyle4.copyWith(color: Colors.white),
+                    )),
               ],
             )
           ]),

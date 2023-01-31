5347baeee8b825970718f4845baea3c32b86a700
Vinicio  Costa Santos <vinicio.csantos@outlook.com>
2023-01-31 08:20:50 -0300

    hotels label implementation

--- lib/scrrens/home_screen.dart
+++ lib/scrrens/home_screen.dart
@@ -91,7 +91,25 @@ class HomeScreen extends StatelessWidget {
                 TicketView(),
                 TicketView(),
               ]),
-            )
+            ),
+            const Gap(15),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Hotels", style: Styles.headLineStyle2),
+                  InkWell(
+                    onTap: () {
+                      print("View all");
+                    },
+                    child: Text("View all",
+                        style: Styles.textStyle
+                            .copyWith(color: Styles.primaryColor)),
+                  ),
+                ],
+              ),
+            ),
           ],
         ));
   }

602b6e4cfce9cd990b954e2e97eb24a312a7ef7c
Vinicio  Costa Santos <vinicio.csantos@outlook.com>
2023-01-30 22:00:16 -0300

    botom side ticket component

--- lib/scrrens/ticket_view.dart
+++ lib/scrrens/ticket_view.dart
@@ -15,7 +15,7 @@ class TicketView extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = AppLayout.getSize(context);
     return SizedBox(
-      width: size.width,
+      width: size.width * 0.80,
       height: 200,
       child: Column(children: [
         Container(
@@ -107,11 +107,92 @@ class TicketView extends StatelessWidget {
                 width: 10,
                 child: DecoratedBox(
                   decoration: BoxDecoration(
+                      color: Colors.white,
                       borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(10),
-                  )),
+                        topRight: Radius.circular(10),
+                        bottomRight: Radius.circular(10),
+                      )),
                 )),
-            Container()
+            Expanded(
+                child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: LayoutBuilder(
+                builder: (BuildContext builder, BoxConstraints constraints) {
+                  return Flex(
+                    direction: Axis.horizontal,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.max,
+                    children: List.generate(
+                        (constraints.constrainWidth() / 15).floor(),
+                        (index) => SizedBox(
+                            width: 3,
+                            height: 1,
+                            child: DecoratedBox(
+                              decoration: BoxDecoration(
+                                  color: Colors.white.withOpacity(0.5)),
+                            ))),
+                  );
+                },
+              ),
+            )),
+            SizedBox(
+                height: 20,
+                width: 10,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        bottomLeft: Radius.circular(10),
+                      )),
+                )),
+          ]),
+        ),
+        Container(
+          margin: const EdgeInsets.only(left: 16),
+          decoration: BoxDecoration(
+            color: Styles.orangeColor,
+            borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(21),
+                bottomRight: Radius.circular(21)),
+          ),
+          padding:
+              const EdgeInsets.only(left: 16, right: 16, top: 10, bottom: 16),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("1 MAY",
+                    style: Styles.headLineStyle3.copyWith(color: Colors.white)),
+                const Gap(5),
+                Text("DATE",
+                    style: Styles.headLineStyle4.copyWith(color: Colors.white)),
+                const Gap(3),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text("8:00 AM",
+                    style: Styles.headLineStyle3.copyWith(color: Colors.white)),
+                const Gap(5),
+                Text("Departure Time",
+                    style: Styles.headLineStyle4.copyWith(color: Colors.white)),
+                const Gap(3),
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text("23",
+                    style: Styles.headLineStyle3.copyWith(color: Colors.white)),
+                const Gap(5),
+                Text("Number",
+                    style: Styles.headLineStyle4.copyWith(color: Colors.white)),
+                const Gap(3),
+              ],
+            ),
           ]),
         )
       ]),

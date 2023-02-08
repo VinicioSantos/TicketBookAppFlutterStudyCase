1ba438215dc4555e5de1a5257760f2d4fb3934fa
Vinicio  Costa Santos <vinicio.csantos@outlook.com>
2023-02-07 22:59:17 -0300

    toggle button

--- lib/scrrens/search_screen.dart
+++ lib/scrrens/search_screen.dart
@@ -10,25 +10,57 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = AppLayout.getSize(context);
     return Scaffold(
       backgroundColor: Styles.bgColor,
       body: ListView(
+        padding: EdgeInsets.symmetric(
+            horizontal: AppLayout.getWidth(20),
+            vertical: AppLayout.getHeight(20)),
         children: [
           Gap(AppLayout.getHeight(40)),
           Text("What are\nyou looking for?",
-              style: Styles.headLineStyle.copyWith(fontSize: 35),
+              style: Styles.headLineStyle
+                  .copyWith(fontSize: AppLayout.getWidth(35)),
               textAlign: TextAlign.start),
+          Gap(AppLayout.getHeight(20)),
           Container(
-            margin: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(20)),
-            child: TextField(
-              decoration: InputDecoration(
-                  hintText: "Search",
-                  hintStyle: Styles.headLineStyle3,
-                  prefixIcon: Icon(Icons.search),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10))),
-            ),
-          )
+              padding: const EdgeInsets.all(3.5),
+              child: Row(children: [
+                /*
+                airlines tickets
+                */
+                Container(
+                    width: size.width * .44,
+                    padding:
+                        EdgeInsets.symmetric(vertical: AppLayout.getHeight(7)),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.horizontal(
+                            left: Radius.circular(AppLayout.getHeight(50))),
+                        color: Colors.white),
+                    child: Center(
+                      child: Text("Airline tickets"),
+                    )),
+                /*
+                Hotels
+                */
+                FittedBox(
+                  child: Container(
+                      width: size.width * .44,
+                      padding: EdgeInsets.symmetric(
+                          vertical: AppLayout.getHeight(7)),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.horizontal(
+                              right: Radius.circular(AppLayout.getHeight(50))),
+                          color: Colors.transparent),
+                      child: Center(
+                        child: Text("Hotels"),
+                      )),
+                ),
+              ]),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(AppLayout.getHeight(50)),
+                  color: const Color(0xFFF4F6FD))),
         ],
       ),
     );
